40922d037ce091013bac5e1ac404718648bc9fc0
unknown <juaneguiabad1639@gmail.com>
2023-12-30 02:56:52 +0100

    Primer commit

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+import { Settings } from './settings.js';
+
+let settings;
+
+// ==========================================================================
+window.onload = () => {
+
+    settings = new Settings();
+
+    const ficha = document.createElement('div');
+    ficha.setAttribute('class', 'ficha');
+
+    settings.doms.tablero.appendChild(ficha);
+
+    for (let i = 0; i < settings.constantes.FILAS; i ++) {
+        for (let ii = 0; ii < settings.constantes.COLUMNAS; ii ++) {
+
+            const id = i.toString() + ii.toString();
+            console.log(id);
+
+            const y = i * settings.constantes.TILE_YY;
+            const x = ii * settings.constantes.TILE_XX;
+
+            const casilla = document.createElement('div');
+            casilla.setAttribute('class', 'casilla');
+            casilla.setAttribute('id', id);
+            casilla.style.width = settings.constantes.TILE_X;
+            casilla.style.height = settings.constantes.TILE_Y;
+            casilla.style.top = y.toString() + 'vw';
+            casilla.style.left = x.toString() + 'vw';
+
+            settings.doms.tablero.appendChild(casilla);
+        }
+    }
+};
---
+++ src/js/settings.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+export class Settings {
+
+    constructor() {
+
+        this.constantes = {
+            TILE_X: '8vw',
+            TILE_Y: '8vw',
+            TILE_XX: 8,
+            TILE_YY: 8,
+            FILAS: 6,
+            COLUMNAS: 7,
+            FPS: 50
+        };
+
+        this.doms = {
+            tablero: document.getElementById('tablero'),
+        }
+    }
+}
